c26eb04ced4659f8bff5cee9ba8aadbe1e1682a4
bf2344 <bford2344@gmail.com>
2021-08-15 08:32:12 -0600

    Update isPalindrome.js

--- isPalindrome/isPalindrome.js
+++ isPalindrome/isPalindrome.js
@@ -1,3 +1,5 @@
+// Determine if a string is a palindrome (reads the same backwards as forwards).
+
 // split the array in half and then compare the first half of the array to the end of the 2nd half of the array. 
 const isPalindrome = str => {
     for (let i = 0; i < str.length / 2; i++) {
