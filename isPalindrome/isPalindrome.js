46e389acdb6ce2ea97fbf244c75ee8c439c7e017
bf2344 <bford2344@gmail.com>
2021-08-15 08:27:21 -0600

    isPalindrome algo

---
+++ isPalindrome/isPalindrome.js
@@ -0,0 +1,9 @@
+// split the array in half and then compare the first half of the array to the end of the 2nd half of the array. 
+const isPalindrome = str => {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+};
