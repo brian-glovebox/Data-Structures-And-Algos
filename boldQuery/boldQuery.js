60ab916a6c90fcd90188b859fe85ccbae310562a
bf2344 <bford2344@gmail.com>
2021-08-31 14:27:17 -0600

    added two more algos

---
+++ boldQuery/boldQuery.js
@@ -0,0 +1,13 @@
+function highlightResult(query, result) {
+    let highlightedResult = [];
+    // build highlighted version of result
+    const splitArray = result.split(" ")
+    for (let i = 0; i < splitArray.length; i++) {
+        let element = splitArray[i];
+        if (element.includes(query)) {
+            element = element.replaceAll(query, `<b>${query}</b>`);
+        }
+        highlightedResult.push(element);
+    }
+    return highlightedResult.join(" ");
+};
